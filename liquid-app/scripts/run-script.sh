b345edbd56ceb55b7ebac0905c58140c7be27b9e
daniel parkes <liquidsmail@gmail.com>
2016-10-27 14:21:49 +0200

    Continuous Integration and deployment with Docker and Ansible

---
+++ liquid-app/scripts/run-script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+case  $1  in
+start)
+exec /usr/sbin/apache2ctl -D FOREGROUND
+;;
+test)
+if [ $(/usr/bin/php /var/www/html/index.php | grep IP | wc -l ) -gt 0 ] ; then
+   echo "PHP app Test Passed OK"
+else
+   echo "PHP app Test Failed" 
+fi
+;;
+*)
+echo "please use $@ start|test"
+;;
+esac
